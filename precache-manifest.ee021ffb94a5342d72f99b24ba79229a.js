52f0cba411c35c08518b1d0393613e00226ac090
Konstantin Bogdanov <kbogdanov1@gmail.com>
2019-05-28 23:51:33 +0300

    Updates

---
+++ precache-manifest.ee021ffb94a5342d72f99b24ba79229a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63fc819b6642857952757153ceeef1d",
+    "url": "/bitcoin-test/index.html"
+  },
+  {
+    "revision": "18cd3500346dc61dabf6",
+    "url": "/bitcoin-test/static/css/main.1bae8569.chunk.css"
+  },
+  {
+    "revision": "249c4df168eade8c14df",
+    "url": "/bitcoin-test/static/js/2.665b0bb4.chunk.js"
+  },
+  {
+    "revision": "18cd3500346dc61dabf6",
+    "url": "/bitcoin-test/static/js/main.2c392d22.chunk.js"
+  },
+  {
+    "revision": "0654a5c225dad9933db9",
+    "url": "/bitcoin-test/static/js/runtime~main.119b9dd0.js"
+  }
+]);
